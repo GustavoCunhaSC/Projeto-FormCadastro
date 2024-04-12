f50469296caed3f5025dea5ad331bd17e4f74db8
Eduardo Tavares Sousa <eduardot910@gmail.com>
2024-04-12 02:05:59 -0300

    Revert "conclusão do dark-mode."
    This reverts commit 6f97ffb91212c6c0d3f0f1353d636c172afc203b.

--- formulario/js/darkmode.js
+++ formulario/js/darkmode.js
@@ -1,23 +1,6 @@
 const $html = document.querySelector('html')
 const $checkbox = document.querySelector('#switch')
 
-var theme = localStorage.getItem("dark")
-$html.classList.add(theme)
-
-if( theme == 'dark-mode'){
-    $checkbox.checked = true
-}
-
 $checkbox.addEventListener('change', function(){
-    if(theme != 'dark-mode'){
-        $html.classList.remove(theme)
-        localStorage.setItem("dark", "dark-mode")
-        theme = localStorage.getItem("dark")
-        $html.classList.add(theme)
-    }else{
-        $html.classList.remove(theme)
-        localStorage.setItem("dark", "linear-mode")
-        theme = localStorage.getItem("dark")
-        $html.classList.add(theme)
-    }
+    $html.classList.toggle('dark-mode')
 })
