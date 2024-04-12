6f97ffb91212c6c0d3f0f1353d636c172afc203b
Eduardo Tavares Sousa <eduardot910@gmail.com>
2024-04-12 01:58:49 -0300

    conclusão do dark-mode.

--- formulario/js/darkmode.js
+++ formulario/js/darkmode.js
@@ -1,6 +1,23 @@
 const $html = document.querySelector('html')
 const $checkbox = document.querySelector('#switch')
 
+var theme = localStorage.getItem("dark")
+$html.classList.add(theme)
+
+if( theme == 'dark-mode'){
+    $checkbox.checked = true
+}
+
 $checkbox.addEventListener('change', function(){
-    $html.classList.toggle('dark-mode')
+    if(theme != 'dark-mode'){
+        $html.classList.remove(theme)
+        localStorage.setItem("dark", "dark-mode")
+        theme = localStorage.getItem("dark")
+        $html.classList.add(theme)
+    }else{
+        $html.classList.remove(theme)
+        localStorage.setItem("dark", "linear-mode")
+        theme = localStorage.getItem("dark")
+        $html.classList.add(theme)
+    }
 })
