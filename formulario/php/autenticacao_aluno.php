bb7db77fc11f125438ddf115ba04366e6403fe9a
LuizFelipe <felipe-developer@outlook.com>
2024-04-21 20:46:28 -0300

    tratamento e validação de dados

--- formulario/php/autenticacao_aluno.php
+++ formulario/php/autenticacao_aluno.php
@@ -6,44 +6,56 @@ echo"<pre>";
 echo"</pre>";
 die();
 /* */
+// Iniciar SESSAO(session_start)
+session_start();
+
+// Verifica se esta chegando dados por POST;
 if(!empty($_POST)){
-  // Obter as informações do formulário ($_POST)
   try {
-    // Prepara as informações
-    // Montar a SQL(pgsql)
-    $sql = "INSERT INTO alunos
-            (nome_alunos, sobrenome_alunos, email_alunos, telefone_alunos, dtnasci_alunos, cpf_alunos, sexo_alunos)
-            VALUES
-            (:nome, :sobrenome, :email, :telefone, :dataDeAniversario, :cpf, :sexo)";
+    // Montar a SQL
+    $sql = "SELECT nome_alunos, sobrenome_alunos, email_alunos, telefone_alunos, dtnasci_alunos, cpf_alunos, sexo_alunos FROM alunos WHERE email_alunos = :email AND senha_aluno = :senha";
 
     // Preparar a SQL(pdo)
     $stmt = $pdo ->prepare($sql);
 
     // Definir/organizar os dados p/SQL
     $dados = array(
-      ':nome' => $_POST['nome'],
-      ':sobrenome' => $_POST['sobrenome'], 
-      ':email'=> $_POST['email'],
-      ':telefone'=> $_POST['telefone'],
-      ':dataDeAniversario' => $_POST['dataDeAniversario'],
-      ':cpf'=> $_POST['cpf'],
-      ':sexo' => $_POST['sexo']
+      ':email' => $_POST['email'],
+      ':senha' => $_POST['senha']
     );
+
+    // Executar a SQL()
+    $stmt -> execute($dados);
     
-    // Executar a SQL(INSERT)
-    // Realizar a inserção das informações no BD
-    if($stmt ->execute($dados)){
-      header("Location: ../login.html?msgSucesso=Cadastro realizado com sucesso!");
+    $result = $stmt->fetchAll();
+    
+    if($stmt->rowCount() == 1){ // Se o resultado da consulta SQL trouxer registro
+      // Autenticação foi realizada com sucesso
+      $result = $result[0];
+      // Definir variaveis da sessao
+      $_SESSION['nome_alunos'] = $result['nome_alunos'];
+      $_SESSION['sobrenome_alunos'] = $result['sobrenome_alunos'];
+      $_SESSION['email_alunos'] = $result['email_alunos'];
+      $_SESSION['telefone_alunos'] = $result['telefone_alunos'];
+      $_SESSION['dtnasci_alunos'] = $result['dtnasci_alunos'];
+      $_SESSION['cpf_alunos'] = $result['cpf_alunos'];
+      $_SESSION['sexo_alunos'] = $result['sexo_alunos'];
+
+      // Redirecionar p/ pagina inicial (ambiente logado)
+      header('Location:./pages/alunos.php');
+    } else{ //Resultado da consulta não trouxe nenhum registro
+      // Senao, retorna falha na autenticação
+      // Destruir sessao
+      session_destroy();
+      // Redirecionar p/ pagina inicial de login
+      header('Location:../login.html?msgErro=Email ou senha inválido');
     }
-    
+
   } catch (PDOException $e) {
-    //throw $th;
-    header("Location: ../cadasto.html?msgErro=Falha ao cadastrar...");
+    die($e->getMessage());
   }
 } else{
-  header('Location: ../cadastro.html?msgErro=Erro de acesso.');
+  header('Location:../login.html?msgErro=Acesso não permitido ');
 }
-die();
-// Redirecionar para a página inicial (logi) c/ mensagem de erro/sucesso
 
 ?>
