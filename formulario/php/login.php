2e5ca668b496307d59b0131dde81a15b3da1dec1
LuizFelipe <felipe-developer@outlook.com>
2024-04-21 18:05:27 -0300

    mapeamento de pastas

--- formulario/php/login.php
+++ formulario/php/login.php
@@ -1,1 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
 <?php echo "helo word"; ?>
+<br>
+<a href="./pages/alunos.php">alunos</a>
+</body>
+</html>
