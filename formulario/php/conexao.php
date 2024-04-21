06010b455bad9c2a1e8e677b044b3f1feaf2307b
LuizFelipe <felipe-developer@outlook.com>
2024-04-21 18:03:13 -0300

    nova conexao

--- formulario/php/conexao.php
+++ formulario/php/conexao.php
@@ -1,7 +1,15 @@
 <?php 
-$conexao = pg_connect("host=localhost port=5432 user=postgres password=1234 dbname=db_elite");
-if (!$conexao){
-  die("conexão com PostgreSQL falhou");
-}
-echo "conexão com PostgreSQL funcionou";
+$endereco = 'localhost';
+$banco = 'db_elite';
+$usuario = 'postgres';
+$senha = '1234';
+
+try {
+  //code...
+  $pdo = new PDO("pgsql:host=$endereco;port=5432;dbname=$banco", $usuario, $senha, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION] );
+  // echo"Conectado ao Banco de dados!!!";
+} catch (PDOException $e) {
+  echo"Falha ao conectar no Banco de dados. </br>"; 
+  die($e -> getMessage());
+} 
 ?>
