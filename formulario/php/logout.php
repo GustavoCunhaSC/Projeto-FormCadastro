02d58f90b4b37b4cb17cc27eb9eae3b8de425a03
LuizFelipe <felipe-developer@outlook.com>
2024-04-21 22:13:50 -0300

    Update logout.php

--- formulario/php/logout.php
+++ formulario/php/logout.php
@@ -1,3 +1,11 @@
 <?php 
+   session_start();
 
+   if(empty($_SESSION)){
+     header("Location: ../login.html?msgErro=Você precisao se autenticar.");
+    } else{
+      session_destroy();
+      header("Location: ../login.html?msgSecesso=Logout efetuado com sucesso!");
+    }
+    die();
 ?>
